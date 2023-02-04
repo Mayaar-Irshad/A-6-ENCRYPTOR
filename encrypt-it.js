142db0ee8a343fd68ee483f4d45ee0caff8d3084
Irshadzed <105467566+Irshadzed@users.noreply.github.com>
2023-02-03 18:04:31 -0800

    first commit

---
+++ encrypt-it.js
@@ -0,0 +1,84 @@
+(function () {
+   "use strict";
+   window.addEventListener("load", init);
+
+   /**
+    * TODO: Write a function comment using JSDoc.
+    */
+   function init() {
+      // Note: In this function, we usually want to set up our event handlers
+      // for UI elements on the page.
+      console.log("Window loaded!");
+      const encryptBtn = document.getElementById("encrypt-it");
+      encryptBtn.addEventListener("click", handleClick);
+      const resetBtn = document.getElementById("reset");
+      resetBtn.addEventListener("click", handleReset);
+      const fontSize = document.querySelectorAll("input[name=text-size]");
+      for (let i = 0; i < fontSize.length; i++) {
+         fontSize[i].addEventListener("change", handleFontChange);
+      }
+   }
+
+   /**
+    * Handles the click event on the "Encrypt-It!" button.
+    */
+
+   /*
+   *  Initializes the page by setting up event handlers for UI elements.
+   */
+   function handleClick() {
+      // Retrieve the text from the textarea
+      const textarea = document.querySelector("#input-text");
+      const inputText = textarea.value;
+
+      // Encrypt the text using the shift cipher
+      const encryptedText = encrypt(inputText);
+
+      // Output the encrypted text to the #result paragraph
+      const result = document.querySelector("#result");
+      result.textContent = encryptedText;
+   }
+
+   /**
+    * Handles the click event on the "Reset" button.
+    */
+   function handleReset() {
+      const textarea = document.querySelector("#input-text");
+      textarea.value = "";
+      const result = document.querySelector("#result");
+      result.textContent = "";
+   }
+
+   /**
+    * Handles the change event on the font size radio buttons.
+    */
+   function handleFontChange() {
+      const textarea = document.querySelector("#input-text");
+      const fontSize = this.value;
+      textarea.style.fontSize = fontSize;
+   }
+
+   /**
+    * Returns an encrypted version of the given text, where
+    * each letter is shifted alphabetically ahead by 1 letter,
+    * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+    * @param {string} text The input text to encrypt.
+    * @return {string} The encrypted text.
+    */
+   function shiftCipher(text) {
+      let result = "";
+      for (let i = 0; i < text.length; i++) {
+         if (text[i] < 'a' || text[i] > 'z') {
+            result += text[i];
+         } else if (text[i] === 'z') {
+            result += 'a';
+         } else {
+            let letter = text.charCodeAt(i);
+            let resultLetter = String.fromCharCode(letter + 1);
+            result += resultLetter;
+         }
+      }
+      return result;
+   }
+
+})();
